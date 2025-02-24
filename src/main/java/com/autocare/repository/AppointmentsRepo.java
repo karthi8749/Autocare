3fd6ca37037fe947f255df1e700410a26acb01b0
Karthikeyan M <119167688+karthi8749@users.noreply.github.com>
2025-02-24 10:14:07 +0530

    Delete src directory

--- src/main/java/com/autocare/repository/AppointmentsRepo.java
+++
@@ -1,8 +0,0 @@
-package com.autocare.repository;
-
-import com.autocare.entity.Appointments;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AppointmentsRepo extends JpaRepository<Appointments, Integer> {
-
-}
--- src/main/java/com/autocare/repository/Emailrepo.java
+++
@@ -1,8 +0,0 @@
-package com.autocare.repository;
-
-import com.autocare.entity.Customers;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Emailrepo extends JpaRepository<Customers, Integer> {
-
-}
